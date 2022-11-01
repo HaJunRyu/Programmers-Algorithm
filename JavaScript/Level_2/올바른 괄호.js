9ae418594a7b7cd0807c75dbe29ab70b80372f8c
HaJunRyu <hjr4557@naver.com>
2022-11-01 18:03:59 +0900

    Complete Level2 올바른 괄호

--- JavaScript/Level_2/올바른 괄호.js
+++ JavaScript/Level_2/올바른 괄호.js
@@ -1,13 +1,13 @@
-// 효율성 테스트 통과 X
+// 효율성 테스트 통과X
 function solution(s) {
   const copyS = s.replace('()', '');
 
   return copyS.includes('()') ? solution(copyS) : copyS.length === 0;
 }
 
-// 효율성 테스트 통과 X
+// 효율성 테스트 통과X
 function solution2(s) {
-  if (s.length % 2 !== 0) return false;
+  if (s.length % 2 !== 0 || s[0] === ')') return false;
 
   let copyS = s;
 
@@ -16,4 +16,18 @@ function solution2(s) {
   }
 
   return copyS.length === 0;
+}
+
+// 이유는 모르겠지만 stack구조로 풀이하니 효율성 테스트 통과 O
+function solution3(s) {
+  if (s.length % 2 !== 0 || s[0] === ')') return false;
+
+  const stack = [];
+
+  for (let bracket of s) {
+    if (bracket === '(') stack.push(s);
+    else stack.pop();
+  }
+
+  return stack.length === 0;
 }
